ee0c1225fc6ab9ab201626d165f8ce52e170b6da
esteban <esteban_silva11@hotmail.com>
2022-03-16 20:28:56 -0300

    use state funcional

--- src/components/CrearDeptoComponent.js
+++ src/components/CrearDeptoComponent.js
@@ -3,18 +3,30 @@ import React, {Component} from 'react';
 import { useState, useEffect, useRef } from "react";
 import DepartamentoServices from '../services/DepartamentoServices';
 import {TextField, Button} from '@mui/material'
-import CrearDepartamento from './CrearDepartamento.js';
 
 
-class CrearDeptoComponent extends Component{
 
-    render(){
-        return(       
-            <div>   
-                <h1> Departamentos </h1>
-                <CrearDepartamento/>
-            </div>   
-        )};
+function CrearDeptoComponent() { 
+    const [nombreDepto, setnombreDepto] = useState("");
+    var nombreOutput = "";
+
+    const mostrarTitulo = () => {
+        nombreOutput = nombreDepto
+        console.log("nombreOutput queda con el valor de:" + nombreOutput + nombreDepto)
+    }
+
+    return(          
+         <div>
+            <h1> Departamentos </h1>
+            <TextField id="nombre-depto" value={nombreDepto} onChange={(e) => setnombreDepto(e.target.value)} label="Nombre Departamento" type="text" />
+            <Button onClick={mostrarTitulo} variant="outlined" >
+                    Crear Departamento 
+            </Button>
+            <div>
+                <h2> {nombreOutput}  </h2>   
+            </div>
+        </div>  
+        )
 };
 
 export default CrearDeptoComponent;
